06cd7788d3c8100b7bbd2bce041204d1b350da08
rosinrae <rosalynrdecker@gmail.com>
2018-03-13 19:21:09 -0600

    added routes, navbar, dummy auth

---
+++ client/src/components/Err404.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Err404 = () => (<h1>ERROR - 404</h1>)
+
+export default Err404;
---
+++ client/src/fakeAuth.js
@@ -0,0 +1,3 @@
+export const isAuthenticated = () => { return localStorage.isAuthenticated }
+export const login = () => { localStorage.setItem("isAuthenticated", "true") }
+export const logout = () => { localStorage.removeItem("isAuthenticated") }
